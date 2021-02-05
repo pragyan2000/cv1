2e8cc670bcb61865ec08c39c5bf7459e6b182ca2
pragyan2000 <75364382+pragyan2000@users.noreply.github.com>
2021-02-05 15:09:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web devloper", "Web devloper","Web devloper"],
+        strings: ["Web developer", "Web developer","Web developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web devloper", "Web devloper","Web devloper"],
+        strings: ["Web developer", "Web developer","Web developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
